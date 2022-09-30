d0054cdf4ecb3da8604b3b44cb40d961297b1dff
KodakBrax <bstone0214@gmail.com>
2022-09-30 13:54:09 -0400

    added onclick listener for clicking yes to eula

--- app/src/main/java/com/example/soteria/MainActivity.kt
+++ app/src/main/java/com/example/soteria/MainActivity.kt
@@ -11,22 +11,29 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val sharedPrefs = getSharedPreferences("eula", Context.MODE_PRIVATE)
+        val sharedPrefs = getSharedPreferences("com.android.soteria", Context.MODE_PRIVATE)
         // check if user has entered the app before
-        sharedPrefs.edit().apply {
-            putBoolean("first_time", true)
-        }.apply()
         if (sharedPrefs.getBoolean("first_time", true)) {
-            val editor = sharedPrefs.edit()
-            editor.apply {
+            sharedPrefs.edit().apply {
                 putBoolean("first_time", false)
             }.apply()
 
             // ask the user to agree to eula
             EulaDialogFragment().show(
                 supportFragmentManager, EulaDialogFragment.TAG)
-        } else {
+        } else if (!sharedPrefs.getBoolean("eula", false)){
+            EulaDialogFragment().show(
+                supportFragmentManager, EulaDialogFragment.TAG)
+        } else { // need an else if to check for permissions
 
         }
+    }
+
+    fun userAgreedToEula() {
+        val sharedPrefs = getSharedPreferences("com.android.soteria", Context.MODE_PRIVATE)
+        sharedPrefs.edit().apply {
+            putBoolean("eula", true)
+        }.apply()
+        // go to get permissions
     }
 }
