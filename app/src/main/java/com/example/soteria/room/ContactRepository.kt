a9aff4b4c619d0f27b41297d0b0a001292f0c9ee
Umar (OSU) <jara.11@buckeyemail.osu.edu>
2022-10-20 21:13:56 -0400

    remaining class-level comments

--- app/src/main/java/com/example/soteria/room/ContactRepository.kt
+++ app/src/main/java/com/example/soteria/room/ContactRepository.kt
@@ -3,8 +3,15 @@ package com.example.soteria.room
 import android.app.Application
 import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
-import com.example.soteria.room.daos.ContactDAO
 import com.example.soteria.room.models.Contact
+
+/*
+Name: ContactRepository
+Description: A Room-compatible repository class to act as SSOT for UI elements displaying
+emergency contacts
+
+Details:
+ */
 
 class ContactRepository (application: Application){
 
--- app/src/main/java/com/example/soteria/room/livedatas/ContactLiveData.kt
+++ app/src/main/java/com/example/soteria/room/livedatas/ContactLiveData.kt
@@ -4,5 +4,9 @@ import android.content.Context
 import androidx.lifecycle.LiveData
 import com.example.soteria.room.models.Contact
 
+/*
+Name: ContactsLiveData
+Description: Class to link Room repository data to updatable UI elements
+ */
 class ContactLiveData(private val mContext: Context) : LiveData<List<Contact>>() {
 }
